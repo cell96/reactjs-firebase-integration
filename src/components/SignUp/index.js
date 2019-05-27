5d2d951ed7ac827abdd3e297af593f80f9dde0c2
Anees <bajwa.anees@gmail.com>
2019-05-27 10:12:45 -0400

    sign up

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -100,7 +100,7 @@ class SignUpFormBase extends Component {
 
 const SignUpLink = () => (
   <p>
-  Don't have an account? <Link to={ROUTES.SIGN_UP}></Link>
+  Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
   </p>
 );
 
